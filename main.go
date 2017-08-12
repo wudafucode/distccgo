98aaf41261b55babb80256ab48b79eb81b990d94
wudafucode <21130046@zju.edu.cn>
2017-08-12 11:38:30 +0800

    完成该函数功能dcc_expand_preprocessor_options

--- main.go
+++ main.go
@@ -3,11 +3,56 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	//"strings"
+	"reflect"
+	"strings"
 )
+
+func copy_extra_args(presultargs* []string,args string)int {
+	 splitargs := strings.Split(args,",")
+	 for i:=1;i<len(splitargs);i++{
+	 	 *presultargs=append(*presultargs,splitargs[i])
+         if strings.HasPrefix(splitargs[i],"-MD") || strings.HasPrefix(splitargs[i],"-MMD"){
+         	*presultargs=append(*presultargs,"-MF")
+         	i++
+         	if(i < len(splitargs)){
+         		*presultargs=append(*presultargs,splitargs[i])
+         	}
+         		
+         	
+         }
+ 	 }
+	return 0
+}
+func dcc_expand_preprocessor_options(argvs []string )int {
+	resultargs:= []string{"hle"}
+
+	for i:=1;i<len(argvs);i++{
+		if strings.HasPrefix(argvs[i],"-Wp,"){
+	        copy_extra_args(&resultargs,argvs[i])
+		}else{
+			resultargs=append(resultargs,argvs[i])
+		}
+
+	}
+	fmt.Println(resultargs)
+    return 0;	
+}
+func dcc_build_somewhere() int{
+
+
+
+
+
+	return 0
+}
 func main(){
-    
+
+     //dcc_expand_preprocessor_options(os.Args);
+     //eturn ;
      fmt.Println(os.Args)
+     t := reflect.TypeOf(os.Args)
+     fmt.Println("Type:",t.Name())
+     return 
      cmd := exec.Command("cc",os.Args[1:]...)
      out,err:=cmd.CombinedOutput()
      if err!= nil{
